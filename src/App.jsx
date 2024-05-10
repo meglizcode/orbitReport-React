d4e1b92d2042e6d7c57d4778af9825ba236eb669
meglizcode <meglizjudd@gmail.com>
2024-05-09 20:59:20 -0500

    first 5 tests passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+import React, {useState} from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newStatDisplay) => {
+      return newStatDisplay.orbitType === currentType;
+    });
+    setSat(displatSats);
+  };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
